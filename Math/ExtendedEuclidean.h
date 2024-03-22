62236dca4208919d4f46c445b0dbd67382caf09e
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-03-22 08:30:43 -0500

    Adding MOD arithmetic

---
+++ Math/ExtendedEuclidean.h
@@ -0,0 +1,23 @@
+/*
+    Solves the ecuation:
+        ax + by = gcd(a, b)
+    Where a, b, and c are constants
+    Solve for x and y
+*/
+
+struct ExtendedEuclidean {
+    int x, a, b, y;
+    ExtendedEuclidean(int a, int b) : b(b), a(a) {}
+    int init() {
+        if (b == 0) {
+            x = 1;
+            y = 0;
+            return a;
+        }
+        ExtendedEuclidean s(b, a%b);
+        int d = s.init();
+        x = s.y;
+        y = s.x - s.y * (a / b);
+        return d;
+    }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# Sources
+- https://github.com/bqi343/cp-notebook
+- https://github.com/pin3da/notebook-generator?tab=readme-ov-file
+- https://github.com/ecnerwala/cp-book
