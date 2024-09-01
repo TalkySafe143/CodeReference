4ed38485c75770ef83f025db2225a8e66529682e
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-09-01 14:23:40 -0500

    Removing Phi func calc in ModNum

--- Math/ModularNumber.h
+++ Math/ModularNumber.h
@@ -1,39 +1,39 @@
 /*
     Computes the Modular Arithmetic
     RT = 5 is primitive root for both common mods
-    Sources: Benq 
+    Sources: Benq
         https://github.com/bqi343/cp-notebook/blob/master/Implementations/content/number-theory%20(11.1)/Modular%20Arithmetic/ModInt.h
     Verification: https://open.kattis.com/problems/modulararithmetic
 */
 
 template<int MOD, int RT> struct mint {
 	static const int mod = MOD;
-	static constexpr mint rt() { 
+	static constexpr mint rt() {
         return RT; // primitive root for FFT
     }
 	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
 	mint() : v(0) {}
-	mint(ll _v) { 
+	mint(ll _v) {
         v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
-		if (v < 0) v += MOD; 
+		if (v < 0) v += MOD;
     }
 
 	bool operator==(const mint& o) const { return v == o.v; }
 	friend bool operator!=(const mint& a, const mint& b) { return !(a == b); }
 	friend bool operator<(const mint& a, const mint& b) { return a.v < b.v; }
-   
-	mint& operator+=(const mint& o) { 
-		if ((v += o.v) >= MOD) v -= MOD; 
-        return *this; 
+
+	mint& operator+=(const mint& o) {
+		if ((v += o.v) >= MOD) v -= MOD;
+        return *this;
     }
-	mint& operator-=(const mint& o) { 
-		if ((v -= o.v) < 0) v += MOD; 
-		return *this; 
+	mint& operator-=(const mint& o) {
+		if ((v -= o.v) < 0) v += MOD;
+		return *this;
     }
 
-	mint& operator*=(const mint& o) { 
-		v = int((ll)v*o.v%MOD); 
-        return *this; 
+	mint& operator*=(const mint& o) {
+		v = int((ll)v*o.v%MOD);
+        return *this;
     }
 	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
 
@@ -43,20 +43,11 @@ template<int MOD, int RT> struct mint {
 	friend mint pow(mint a, ll p) {
 		mint ans = 1; assert(p >= 0);
 		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
-		return ans; 
+		return ans;
     }
 
-	friend mint inv(const mint& a) { assert(a.v != 0); 
-		return pow(a,MOD-2); 
-    }
-
-    friend mint phiInv(const mint&a) {
-        ExtendedEuclidean s(a, MOD);
-        int g = s.init()
-        if (g != 1) return mint(-1);
-
-        s.x = (x%MOD + MOD) % MOD;
-        return mint(s.x);
+	friend mint inv(const mint& a) { assert(a.v != 0);
+		return pow(a,MOD-2);
     }
 
 	mint operator-() const { return mint(-v); }
