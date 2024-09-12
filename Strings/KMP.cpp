ce1c7e441ec3224cf3112a618fb15ea2788b6f7b
Sebastian Galindo <74377523+TalkySafe143@users.noreply.github.com>
2024-09-12 10:39:22 -0500

    KMP

---
+++ Strings/KMP.cpp
@@ -0,0 +1,20 @@
+/*
+  Prefix function to KMP
+  Source: Me
+
+  Verification: *
+  Complexity: O(|P|)
+**/
+
+vi prefixFunction(string P) {
+	vi s(len(P));
+	s[0] = 0;
+	int border = 0;
+	for (int i = 1; i < len(P); i++) {
+		while (border > 0 && P[i] != P[border]) border = s[border-1];
+		if (P[i] == P[border]) border++;
+		else border = 0;
+		s[i] = border;
+	}
+	return s;
+}
