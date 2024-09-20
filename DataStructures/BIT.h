c1b0b42935bc672b9b5bc800b3fad27f7e856133
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-09-19 21:39:49 -0500

    CHange ModNum

--- DataStructures/BIT.h
+++ DataStructures/BIT.h
@@ -1,6 +1,6 @@
 /**
  * Fenwick Tree
- * 
+ *
  * Sources: Cp-algorithms
  *  - https://cp-algorithms.com/data_structures/fenwick.html#
  *
@@ -19,12 +19,12 @@ struct BITZero { // Get and Range query - Zero-based indexing - Linear Construct
     BITZero(vi &a) : BITZero(len(a)) {
         forn(i, n) { bit[i] += a[i]; int r = i | (i+1); if (r < n) bit[r] += bit[i]; }
     }
-    
-    // Going down in tree to get sum 
+
+    // Going down in tree to get sum
     int sum(int i) { int ans = 0; for (; i >= 0; i = (i & (i+1))-1) ans += bit[i]; return ans; }
-    
+
     // Going up in tree to update
-    void update(int i, int delta) { for (; i < n; i = i | (idx+1)) bit[i] += delta; }
+    void update(int i, int delta) { for (; i < n; i = i | (i+1)) bit[i] += delta; }
 
     int rangeSum(int i, int j) { return sum(j)-sum(i); };
 }
@@ -33,7 +33,7 @@ struct BITZero { // Get and Range query - Zero-based indexing - Linear Construct
 struct BIT { // Get and Range query - One-based indexing
     int n;
     vi bit;
-    
+
     BIT() : n(0) {};
 
     BIT(int n) {
@@ -44,14 +44,14 @@ struct BIT { // Get and Range query - One-based indexing
     BIT(vi &a) : BIT(len(a)) {
         forn(i, n) update(i, a[i]);
     }
-    
+
     void update(int i, int delta) {
         while (i < n) {
             bit[i] += delta;
             i += (i&(-i)); // Go up in the tree by LB
         }
     }
-    
+
     // Compute the prefix sum [1, i]
     int sum(int i) {
         int ans = 0;
@@ -73,10 +73,10 @@ struct BITUpdate { // Range Update and Get query
     vi bit;
 
     BITUpdate(int n) {
-        this->n = n;
-        bit.assign(n, 0);
+        this->n = n+1;
+        bit.assign(n+1, 0);
     }
-    
+
     BITUpdate(vi &a) : BITUpdate(len(a)) {
         forn(i, n) updateRange(i, i, a[i]);
     }
@@ -105,9 +105,9 @@ struct BITUpdate { // Range Update and Get query
 struct BITRQU { // Range Update and Range Query
     int n;
     BIT a, b;
-    
+
     BITRQU(int n) {
-        this->n = n;
+        this->n = n+1;
         a = BIT(n); b = BIT(n);
     }
 
@@ -121,6 +121,6 @@ struct BITRQU { // Range Update and Range Query
     }
 
     int prefixSum(int i) { return a.sum(i)*i - b.sum(i); }
-    
+
     int rangeSum(int l, int r) { return prefixSum(r)-prefixSum(l); };
 };
--- Math/ModularNumber.h
+++ Math/ModularNumber.h
@@ -8,53 +8,23 @@
 
 template<int MOD, int RT> struct mint {
 	static const int mod = MOD;
-	static constexpr mint rt() {
-        return RT; // primitive root for FFT
-    }
-	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
-	mint() : v(0) {}
-	mint(ll _v) {
-        v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
-		if (v < 0) v += MOD;
-    }
-
-	bool operator==(const mint& o) const { return v == o.v; }
-	friend bool operator!=(const mint& a, const mint& b) { return !(a == b); }
-	friend bool operator<(const mint& a, const mint& b) { return a.v < b.v; }
-
-	mint& operator+=(const mint& o) {
+	static constexpr mint rt() { return RT; } // primitive root
+ 	int v;
+ 	explicit operator int() const { return v; }
+	mint():v(0) {}
+	mint(ll _v):v(int(_v%MOD)) { v += (v<0)*MOD; }
+	mint& operator+=(mint o) {
 		if ((v += o.v) >= MOD) v -= MOD;
-        return *this;
-    }
-	mint& operator-=(const mint& o) {
+		return *this; }
+	mint& operator-=(mint o) {
 		if ((v -= o.v) < 0) v += MOD;
-		return *this;
-    }
-
-	mint& operator*=(const mint& o) {
-		v = int((ll)v*o.v%MOD);
-        return *this;
-    }
-	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
-
-    /*
-        Computes the Binary Exponentation with MOD
-    */
-	friend mint pow(mint a, ll p) {
-		mint ans = 1; assert(p >= 0);
-		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
-		return ans;
-    }
-
-	friend mint inv(const mint& a) { assert(a.v != 0);
-		return pow(a,MOD-2);
-    }
-
-	mint operator-() const { return mint(-v); }
-	mint& operator++() { return *this += 1; }
-	mint& operator--() { return *this -= 1; }
-	friend mint operator+(mint a, const mint& b) { return a += b; }
-	friend mint operator-(mint a, const mint& b) { return a -= b; }
-	friend mint operator*(mint a, const mint& b) { return a *= b; }
-	friend mint operator/(mint a, const mint& b) { return a /= b; }
+		return *this; }
+	mint& operator*=(mint o) {
+		v = int((ll)v*o.v%MOD); return *this; }
+	friend mint pow(mint a, ll p) { assert(p >= 0);
+		return p==0?1:pow(a*a,p/2)*(p&1?a:1); }
+	friend mint inv(mint a) { assert(a.v != 0); return pow(a,MOD-2); }
+	friend mint operator+(mint a, mint b) { return a += b; }
+	friend mint operator-(mint a, mint b) { return a -= b; }
+	friend mint operator*(mint a, mint b) { return a *= b; }
 };
