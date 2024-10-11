b7e3e203e0b39f9e223b0ea8b245aa6940d767ac
Sebastian Galindo <74377523+TalkySafe143@users.noreply.github.com>
2024-10-11 06:40:16 -0500

    Fix bug Cycle Finding.h

--- Graphs/Cycle Finding.h
+++ Graphs/Cycle Finding.h
@@ -33,8 +33,8 @@ struct CycleDetection {
     }
 
     CycleDetection(vi adj[], int n) {
-        color.assign(n, 0);
-        parent.assign(n, -1);
+        color.assign(n+1, 0);
+        parent.assign(n+1, -1);
         cs = -1;
         forn(i, n) if (color[i+1] == 0 && dfs(i+1, adj)) break;
 
