c1e30d12fa47e0caf14c03ce20e0d7e280205e1c
Sebastian Galindo <74377523+TalkySafe143@users.noreply.github.com>
2025-02-15 12:21:49 -0500

    Create Euler.h - Missing some validations path/cycle

---
+++ Graphs/Euler.h
@@ -0,0 +1,49 @@
+struct Euler {
+	int n; 
+	vi path, indeg, outdeg, S, T;
+	bool exist = false;
+	vector<vi> g;
+
+	Euler(vector<vi> &adj, int sz) : n(sz), g(adj) {
+		indeg.assign(n+1, 0);
+		outdeg.assign(n+1, 0);
+		forn(i, n) {
+			int u = i+1;
+			outdeg[u] = len(adj[u]);
+			for (auto v: adj[u]) indeg[v]++;
+		}
+		int badV = 0;
+		forn(i, n) {
+			int u = i+1;
+			if (outdeg[u] == indeg[u]+1) S.pb(u);
+			if (indeg[u] == outdeg[u]+1) T.pb(u);
+			if (indeg[u] != outdeg[u]) badV++;
+		}
+
+		if (badV <= 2 and len(S) <= 1 and len(T) <= 1) exist = true;
+	};	
+
+	void construct() {
+		if (!exist) return;
+		stack<int> st; st.push((len(S) ? S.back() : 1));
+		while (len(st)) {
+			int u = st.top();
+			if (!len(g[u])) {
+				path.pb(u);
+				st.pop();
+			} else {
+				st.push(g[u].back());
+				g[u].pop_back();
+			}
+		}
+
+		forn(i, n) {
+			int u = i+1;
+			if (len(g[u])) {
+				exist = false;
+				return;
+			}
+		}
+		reverse(all(path));
+	}
+};
