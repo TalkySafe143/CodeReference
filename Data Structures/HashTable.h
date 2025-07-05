e79371f0d1e4adcd9ac60df5b873018c3e073d73
Sebastian Galindo <sebastian.galindo143@gmail.com>
2025-07-05 09:06:04 -0500

    Add: Hash Table

---
+++ Data Structures/HashTable.h
@@ -0,0 +1,20 @@
+/*
+Hash table with custom hash to safe $O(1)$
+---
+Source: Own
+*/
+
+
+using namespace __gnu_pbds;
+ 
+struct splitmix64 {
+    size_t operator()(size_t x) const {
+        static const size_t fixed = chrono::steady_clock::now().time_since_epoch().count();
+        x += 0x9e3779b97f4a7c15 + fixed;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+};
+
+using hashTable = gp_hash_table<int, int, splitmix64>;
