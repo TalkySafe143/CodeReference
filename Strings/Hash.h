a796a8765a0527b7cc38a1f688dad554ce5beb73
Sebastian Galindo <74377523+TalkySafe143@users.noreply.github.com>
2025-10-01 13:07:07 -0500

    Update Hash String (2 modulo)

--- Strings/Hash.h
+++ Strings/Hash.h
@@ -2,31 +2,69 @@
 Computes the Hash and make range queries of the hash.\\
 For build $O(|s|)$ and $O(1)$ per query.
 */
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
 
-class HashedString {
-  private:
-	static const ll M = 1e9 + 9;
-	static const ll B = 9973;
- 
-	// pow[i] contains B^i % M
-	static vl pow;
- 
-	// p_hash[i] is the hash of the first i characters of the given string
-	vl p_hash;
- 
-  public:
-	HashedString(const string &s) : p_hash(len(s) + 1) {
-		while (len(pow) < len(s)) { pow.pb((pow.back() * B) % M); } 
-		p_hash[0] = 0;
-		forn(i, len(s)) {
-			p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
-		}
-	}
- 
-	ll get_hash(int start, int end) {
-		long long raw_val =
-		    (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
-		return (raw_val % M + M) % M;
-	}
+constexpr int N = 4e5 + 5;
+
+constexpr int MOD1 = 127657753, MOD2 = 987654319;
+const int b_first = uniform_int_distribution<int>(0, MOD1 - 1)(rng),
+          b_second = uniform_int_distribution<int>(0, MOD2 - 1)(rng);
+pair<int, int> pw[N], ipw[N];
+
+int binpow(long long a, long long b, long long m) {
+  long long res = 1;
+  a %= m;
+  while (b) {
+    if (b & 1)
+      res = (long long)res * a % m;
+    a = (long long)a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+void precalc_hash() {
+  pw[0] = {1, 1};
+  for (int i = 1; i < N; i++) {
+    pw[i].first = 1LL * pw[i - 1].first * b_first % MOD1;
+    pw[i].second = 1LL * pw[i - 1].second * b_second % MOD2;
+  }
+
+  int inverse_b_first = binpow(b_first, MOD1 - 2, MOD1);
+  int inverse_b_second = binpow(b_second, MOD2 - 2, MOD2);
+
+  ipw[0] = {1, 1};
+  for (int i = 1; i < N; i++) {
+    ipw[i].first = 1LL * ipw[i - 1].first * inverse_b_first % MOD1;
+    ipw[i].second = 1LL * ipw[i - 1].second * inverse_b_second % MOD2;
+  }
+}
+
+struct HashString {
+
+  int n;
+  string s;
+  vector<pair<int, int>> hs; // 1-index
+  HashString() = default;
+
+  HashString(string &str) {
+    n = (int)str.size();
+    s = str;
+    hs.emplace_back(0, 0);
+    for (int i = 0; i < n; i++) {
+      pair<int, int> p;
+      p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD2) % MOD1;
+      p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD1;
+      hs.push_back(p);
+    }
+  }
+
+  pair<int, int> get_hash(int l, int r) { // 1-based
+    pair<int, int> ans;
+    ans.first =
+        (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
+    ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL *
+                 ipw[l - 1].second % MOD2;
+    return ans;
+  }
 };
-vl HashedString::pow = {1};
