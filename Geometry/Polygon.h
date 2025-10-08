171c403e11e644203f3478f1953de24eb6a77b93
Sebastian Galindo <sebastian.galindo143@gmail.com>
2025-10-08 18:54:56 -0500

    Add convex hull

--- Geometry/Polygon.h
+++ Geometry/Polygon.h
@@ -9,7 +9,6 @@ typedef long double T;
 const T EPS = 1e-8L;
 const T E0 = 0.0L; // Keep 0 for integer, otherwise EPS
 
-
 // Dado un conjunto de puntos, ver si es convexo
 bool isConvex(vector<Point> &polygon) {
   int n = len(polygon);
@@ -54,4 +53,63 @@ bool pointInPolygon(Point a, vector<Point> &p, bool strict = true) {
     cnt += crossesRay(p[i], p[(i + 1) % n], a);
   }
   return cnt & 1;
+}
+
+vector<Point> convex_hull_collinear(vector<Point> v) {
+  vector<Point> hull;
+
+  sort(v.begin(), v.end(), [&](const Point &a, const Point &b) {
+    return make_pair(a.x, a.y) < make_pair(b.x, b.y);
+  });
+
+  int rep = 2;
+  int sz = 0;
+  while (rep--) {
+    for (auto i : v) {
+      while ((int)hull.size() - sz > 1 and
+             orient(end(hull)[-2], hull.back(), i) > 0) {
+        hull.pop_back();
+      }
+      hull.push_back(i);
+    }
+    hull.pop_back();
+    sz = (int)hull.size();
+    reverse(v.begin(), v.end());
+  }
+
+  return hull;
+}
+
+// This convex hull do not take any collinear points
+vector<Point> buildHull(vector<Point> v, bool allow_collinear) {
+
+  if ((int)v.size() < 3)
+    return v;
+
+  int ind = int(min_element(v.begin(), v.end(),
+                            [&](const Point &a, const Point &b) {
+                              return make_pair(a.x, a.y) < make_pair(b.x, b.y);
+                            }) -
+                v.begin());
+  vector<Point> hull{v[ind]};
+  vector<int> candidate;
+
+  for (int i = 0; i < (int)v.size(); i++)
+    if (v[i] != v[ind])
+      candidate.push_back(i);
+
+  sort(candidate.begin(), candidate.end(), [&](int a, int b) {
+    T o = orient(v[ind], v[a], v[b]);
+    return o != 0 ? o > 0 : (v[a] - v[ind]).sq() < (v[b] - v[ind]).sq();
+  });
+
+  for (auto i : candidate) {
+    while ((int)hull.size() > 1 and
+           orient(end(hull)[-2], hull.back(), v[i]) <= 0) {
+      hull.pop_back();
+    }
+    hull.push_back(v[i]);
+  }
+
+  return hull;
 }
